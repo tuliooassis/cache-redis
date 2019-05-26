041c1b2013bbbb187902e5c8a065a2b1e63add10
tuliooassis <tulioassis@outlook.com>
2019-05-26 20:33:53 -0300

    removed useless files

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -11,7 +10,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/cache', cacheRouter);
 
